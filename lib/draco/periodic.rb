942bc69bd6ddc8a600d53d5d3078b8a127d4fb17
Matt Pruitt <matt@guitsaru.com>
2021-01-08 19:24:02 +0700

    Initial commit

---
+++ lib/draco/periodic.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "periodic/version"
+
+module Draco
+  module Periodic
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
